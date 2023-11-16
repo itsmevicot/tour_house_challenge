f713e562acb6e6dd0b8b6a3d134771847c8418bb
Victor <victorchagasms@gmail.com>
2023-11-16 02:28:35 -0300

    fix: correção no manager e comentando models de país, cidade e estado

--- base/manager.py
+++ base/manager.py
@@ -4,18 +4,18 @@ from django.db import models
 class ActiveManager(models.Manager):
     def actives(self, **kwargs):
         """
-        Returns a queryset containing all instances of the model where 'active' is True.
+        Returns a queryset containing all instances of the model where 'is_active' is True.
         Raises a ValueError if 'active' is provided as a keyword argument.
         """
-        if 'active' in kwargs:
-            raise ValueError('Cannot override the value of "active" in actives.')
-        return self.get_queryset().filter(active=True, **kwargs)
+        if 'is_active' in kwargs:
+            raise ValueError('Cannot override the value of "is_active" in actives.')
+        return self.get_queryset().filter(is_active=True, **kwargs)
 
     def inactives(self, **kwargs):
         """
-        Returns a queryset containing all instances of the model where 'active' is False.
+        Returns a queryset containing all instances of the model where 'is_active' is False.
         Raises a ValueError if 'active' is provided as a keyword argument.
         """
-        if 'active' in kwargs:
-            raise ValueError('Cannot override the value of "active" in inactives.')
-        return self.get_queryset().filter(active=False, **kwargs)
+        if 'is_active' in kwargs:
+            raise ValueError('Cannot override the value of "is_active" in inactives.')
+        return self.get_queryset().filter(is_active=False, **kwargs)
--- base/models.py
+++ base/models.py
@@ -11,70 +11,70 @@ class BaseModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-    active = models.BooleanField(default=True, editable=False)
+    is_active = models.BooleanField(default=True, editable=False)
 
     class Meta:
         abstract = True
 
     def delete(self):
-        self.active = False
+        self.is_active = False
         self.save()
 
 
-class Country(BaseModel):
-    name = models.CharField(
-        max_length=100
-    )
-    iso3 = models.CharField(
-        max_length=3,
-        null=True,
-        blank=True
-    )
-
-    class Meta:
-        verbose_name = 'Country'
-        verbose_name_plural = 'Countries'
-
-    def __str__(self):
-        return self.name
-
-
-class State(BaseModel):
-    name = models.CharField(
-        max_length=100
-    )
-    country = models.ForeignKey(
-        "base.Country",
-        related_name='states',
-        on_delete=models.PROTECT
-    )
-
-    class Meta:
-        verbose_name = 'State'
-        verbose_name_plural = 'States'
-
-    def __str__(self):
-        return f"{self.name}, {self.country.name}"
-
-
-class City(BaseModel):
-    name = models.CharField(
-        max_length=100
-    )
-    state = models.ForeignKey(
-        "base.State",
-        related_name='cities',
-        on_delete=models.PROTECT
-    )
-    state_code = models.CharField(
-        max_length=3,
-        null=True,
-        blank=True
-    )
-
-    class Meta:
-        verbose_name = 'City'
-        verbose_name_plural = 'Cities'
-
-    def __str__(self):
-        return f"{self.name}, {self.state.name}, {self.state.country.name}"
+# class Country(BaseModel):
+#     name = models.CharField(
+#         max_length=100
+#     )
+#     iso3 = models.CharField(
+#         max_length=3,
+#         null=True,
+#         blank=True
+#     )
+#
+#     class Meta:
+#         verbose_name = 'Country'
+#         verbose_name_plural = 'Countries'
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class State(BaseModel):
+#     name = models.CharField(
+#         max_length=100
+#     )
+#     country = models.ForeignKey(
+#         "base.Country",
+#         related_name='states',
+#         on_delete=models.PROTECT
+#     )
+#
+#     class Meta:
+#         verbose_name = 'State'
+#         verbose_name_plural = 'States'
+#
+#     def __str__(self):
+#         return f"{self.name}, {self.country.name}"
+#
+#
+# class City(BaseModel):
+#     name = models.CharField(
+#         max_length=100
+#     )
+#     state = models.ForeignKey(
+#         "base.State",
+#         related_name='cities',
+#         on_delete=models.PROTECT
+#     )
+#     state_code = models.CharField(
+#         max_length=3,
+#         null=True,
+#         blank=True
+#     )
+#
+#     class Meta:
+#         verbose_name = 'City'
+#         verbose_name_plural = 'Cities'
+#
+#     def __str__(self):
+#         return f"{self.name}, {self.state.name}, {self.state.country.name}"
