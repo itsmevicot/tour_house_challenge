c15e55b50f9d1134b1d62bcd14afd16f7a7420e4
Victor <victorchagasms@gmail.com>
2023-11-16 02:27:32 -0300

    feat: serializers para autenticação e validação de campos

--- apis/companies/serializers.py
+++ apis/companies/serializers.py
@@ -6,3 +6,7 @@ class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+    def validate_cnpj(self, value):
+        cleaned_cnpj = value.translate(str.maketrans('', '', './-'))
+        return cleaned_cnpj
--- base/serializers.py
+++ base/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from base.models import Country, State, City
 
 
