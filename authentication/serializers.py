46637c363e584fbc589fa24f60f084d75e577a97
Victor <victorchagasms@gmail.com>
2023-11-17 01:37:31 -0300

    feat: validações de senha do usuário base

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
 from .models import BaseUser
+import re
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -11,8 +12,20 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         model = BaseUser
         fields = ['email', 'password']
 
+    def validate_password(self, value):
+        validate_password(value)
+
+        if not any(char.isdigit() for char in value) or not any(char.isalpha() for char in value):
+            raise serializers.ValidationError("A senha deve ser alfanumérica.")
+
+        if not re.findall('[^A-Za-z0-9]', value):
+            raise serializers.ValidationError("A senha deve conter pelo menos um caractere especial.")
+
+        return value
+
     def create(self, validated_data):
-        return BaseUser.objects.create_user(**validated_data)
+        user = BaseUser.objects.create_user(**validated_data)
+        return user
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
