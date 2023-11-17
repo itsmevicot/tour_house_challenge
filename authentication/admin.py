6de58d642e7419c391f24287709fa1aa58171261
Victor <victorchagasms@gmail.com>
2023-11-17 13:55:41 -0300

    feat: tela de admin com action para ativar usuário(s)

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'is_active', 'is_superuser']
+    ordering = ['email']
+    actions = ['make_active']
+
+    @admin.action(description=_('Activate selected users'))
+    def make_active(self, request, queryset):
+        updated = queryset.update(is_active=True)
+        self.message_user(request, _('%d users were successfully activated.' % updated))
